1e1e684009b8d55d0d6582f8a1c4d633133a1574
dev-JoaoCintra1 <joao.gabrielccosta9@gmail.com>
2026-09-24 21:05:25 -0300

    feat/ Componente de NavBar

---
+++ frontend/src/pages/Home/Paginas/Produtos.tsx
@@ -0,0 +1,10 @@
+// src/pages/Produtos/Produtos.tsx
+const Produtos = () => {
+  return (
+    <div className="min-h-screen bg-reuso-canvas p-8">
+      <h1 className="text-2xl font-bold text-reuso-text">Produtos</h1>
+    </div>
+  );
+};
+
+export default Produtos;
